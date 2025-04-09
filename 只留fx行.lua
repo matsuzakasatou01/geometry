8a20dea8a3f9e1422f9697a4229e668b3e75a6a8
matsuzakasatou01 <zhangchengjun9936@qq.com>
2025-04-09 19:19:09 +0800

    Add files via upload

---
+++ 只留fx行.lua
@@ -0,0 +1,24 @@
+local get = aegisub.gettext
+
+script_name = get "只留fx行"
+script_description = get "只留fx行"
+script_author = "霜庭云花Sub  松坂さとう"
+script_version = "1.0"
+
+function fx(subs)
+    for i = 1,#subs do
+        if subs[i].section == "[Events]" then
+            a = i
+            break
+        end
+    end
+    for i = 1,#subs do
+        if subs[i].section == "[Events]" and subs[i].effect == "fx" then
+            b = i - 1
+            break
+        end
+    end
+    subs.deleterange(a,b)
+end
+
+aegisub.register_macro(script_name,script_description,fx)
