bdd823257089090fd7e9a9fc180cc5b23bd4b637
matsuzakasatou01 <zhangchengjun9936@qq.com>
2025-06-16 19:08:54 +0800

    v2.0
    简化代码

--- 绘图坐标取整.lua
+++ 绘图坐标取整.lua
@@ -3,21 +3,15 @@ local get = aegisub.gettext
 script_name = get "绘图坐标取整"
 script_description = get "把选中行的绘图坐标取整"
 script_author = "松坂さとう"
-script_version = "1.0"
-
-function round(num,decimal)
-    decimal = decimal or 0
-    local mult = 10^decimal
-    return math.floor(num*mult + 0.5)/mult
-end
+script_version = "2.0"
 
 function shape_round(subs,sel)
     for _,i in ipairs(sel) do
         local line = subs[i]
         line.text = string.gsub(line.text,"([-.%d]+) ([-.%d]+)",
         function (x,y)
-            x = round(tonumber(x))
-            y = round(tonumber(y))
+            x = math.floor(x + 0.5)
+            y = math.floor(y + 0.5)
             return string.format("%s %s",x,y)
         end)
         subs[i] = line
