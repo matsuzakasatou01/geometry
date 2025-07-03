561376c6662bf497d3d32728ff17dc973d44d701
matsuzakasatou01 <zhangchengjun9936@qq.com>
2025-07-03 09:23:01 +0800

    v1.0
    添加重置karaoke行脚本

---
+++ 重置karaoke行.lua
@@ -0,0 +1,24 @@
+local get = aegisub.gettext
+
+script_name = get "重置karaoke行"
+script_description = get "重置karaoke行"
+script_author = "松坂さとう"
+script_version = "1.0"
+
+function reset_karaoke(subs)
+    for i = 1,#subs do
+        local l = subs[i]
+        if l.section == "[Events]" and subs[i].effect == "fx" then
+            break
+        end
+        if l.section == "[Events]" and subs[i].effect == "karaoke" then
+        l.text = l.text:gsub("{[^}]+}","")
+        l.comment = false
+        l.actor = ""
+        l.effect = ""
+        end
+        subs[i] = l
+    end
+end
+
+aegisub.register_macro(script_name,script_description,reset_karaoke)
