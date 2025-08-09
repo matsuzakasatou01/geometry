57e11c026c5dc1d5bf79c6153f21e03393facc77
matsuzakasatou01 <zhangchengjun9936@qq.com>
2025-08-09 17:38:53 +0800

    v1.1
    优化代码

--- 重置karaoke行.lua
+++ 重置karaoke行.lua
@@ -3,19 +3,19 @@ local get = aegisub.gettext
 script_name = get "重置karaoke行"
 script_description = get "重置karaoke行"
 script_author = "松坂さとう"
-script_version = "1.0"
+script_version = "1.1"
 
 function reset_karaoke(subs)
     for i = 1,#subs do
         local l = subs[i]
-        if l.section == "[Events]" and subs[i].effect == "fx" then
+        if l.section == "[Events]" and l.effect == "fx" then
             break
         end
-        if l.section == "[Events]" and subs[i].effect == "karaoke" then
-        l.text = l.text:gsub("{[^}]+}","")
-        l.comment = false
-        l.actor = ""
-        l.effect = ""
+        if l.section == "[Events]" and l.effect == "karaoke" then
+            l.text = l.text:gsub("{[^}]+}","")
+            l.comment = false
+            l.actor = ""
+            l.effect = ""
         end
         subs[i] = l
     end
